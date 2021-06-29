5584926627a06b8eebf9d9dcd4de1d7e320894ca
David Price <thedavid@thedavidprice.com>
2021-06-29 14:57:50 -0700

    setup graphql-server; logger; v0.34.2-canary.35

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -2,13 +2,15 @@ import {
   createGraphQLHandler,
   makeMergedSchema,
   makeServices,
-} from '@redwoodjs/api'
+} from '@redwoodjs/graphql-server'
 
 import schemas from 'src/graphql/**/*.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
+import { logger } from 'src/lib/logger'
 import { db } from 'src/lib/db'
 
 export const handler = createGraphQLHandler({
+  logger,
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
@@ -16,5 +18,5 @@ export const handler = createGraphQLHandler({
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
-  }
+  },
 })
