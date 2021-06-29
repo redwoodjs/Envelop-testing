a8a1c596c23ee618174698f85b231c50196a5085
David Price <thedavid@thedavidprice.com>
2021-06-29 15:16:49 -0700

    upgrade 0.34.2-canary.35; add logger DB + GraphQL

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -1,3 +1,21 @@
+// See https://github.com/prisma/prisma2/blob/master/docs/prisma-client-js/api.md#constructor
+// for options.
+
 import { PrismaClient } from '@prisma/client'
 
-export const db = new PrismaClient()
+import { emitLogLevels, handlePrismaLogging } from '@redwoodjs/api/logger'
+
+import { logger } from './logger'
+
+/*
+ * Instance of the Prisma Client
+ */
+export const db = new PrismaClient({
+  log: emitLogLevels(['info', 'warn', 'error']),
+})
+
+handlePrismaLogging({
+  db,
+  logger,
+  logLevels: ['info', 'warn', 'error'],
+})
