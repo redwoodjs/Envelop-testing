1d068eedcc78fca42b150ee4f009499efe423e15
A. David Thyresson <dthyresson@gmail.com>
2021-07-03 11:17:42 -0400

    Setup DataDog logging

--- api/src/lib/logger.js
+++ api/src/lib/logger.js
@@ -1,6 +1,30 @@
 import { createLogger } from '@redwoodjs/api/logger'
 
 /**
+ * Stream logs to Datadog
+ */
+import datadog from 'pino-datadog'
+
+const isDataDogSetup =
+  process.env.DATADOG_API_KEY && process.env.DATADOG_API_KEY !== ''
+
+/**
+
+ * Creates a synchronous pino-datadog stream
+ *
+ * @param {object} options - Datadog options including your account's API Key
+ *
+ * @typedef {DestinationStream}
+ */
+export const stream = datadog.createWriteStreamSync({
+  apiKey: process.env.DATADOG_API_KEY,
+  ddsource: 'main',
+  ddtags: 'main',
+  service: 'envelop-testing',
+  size: 1,
+})
+
+/**
  * Creates a logger with RedwoodLoggerOptions
  *
  * These extend and override default LoggerOptions,
@@ -15,5 +39,6 @@ import { createLogger } from '@redwoodjs/api/logger'
  * @param {boolean} showConfig - whether to display logger configuration on initialization
  */
 export const logger = createLogger({
-  options: { level: 'info', prettyPrint: true },
+  options: { level: 'info', prettyPrint: false },
+  destination: isDataDogSetup && stream,
 })
