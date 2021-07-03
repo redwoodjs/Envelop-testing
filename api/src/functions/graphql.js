7a917f73244592b83c1ad33d9e0943ad2f513a7d
A. David Thyresson <dthyresson@gmail.com>
2021-07-03 11:52:49 -0400

    Setup netlify envelop logging

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -2,15 +2,24 @@ import {
   createGraphQLHandler,
   makeMergedSchema,
   makeServices,
-} from '@redwoodjs/graphql-server'
+} from '@redwoodjs/api'
 
 import schemas from 'src/graphql/**/*.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
-import { logger } from 'src/lib/logger'
 import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
-  loggerConfig: { logger, options: {} },
+  loggerConfig: {
+    logger,
+    options: {
+      operationName: true,
+      userAgent: true,
+      data: true,
+      requestId: true,
+      tracing: true,
+    },
+  },
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
